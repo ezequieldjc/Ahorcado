95e46b66982ef666586ac874f2ca9a1a149e3f0d
gabicapitanelli <gabrielcapitanelli@hotmail.com>
2020-06-26 23:01:20 -0300

    Navegacion

--- project/models.py
+++ project/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy 
 from sqlalchemy import Column, ForeignKey, Integer, String, Time, Date, DateTime
 from sqlalchemy.sql import func
+from datetime import timedelta, datetime
 import random
 
 
@@ -11,7 +12,7 @@ class Jugadas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30))
     puntaje = db.Column(db.Integer)
-    fecha = Column(DateTime(timezone=True), nullable=False, default=func.now())
+    fecha = Column(DateTime(timezone=True), nullable=False, default=func.now()+ timedelta(hours=-3))
 
 class Universo(db.Model):
     __tablename__ = 'universo'
