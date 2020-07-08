2a4c2ef083fdb1e9ffab1e27888c191c5c125354
Gabriel Capitanelli <gabrielcapitanelli@hotmail.com>
2020-07-08 09:58:28 -0300

    Test driver

--- features/environment.py
+++ features/environment.py
@@ -35,7 +35,7 @@ def before_all(context):
     context.pa_app = threading.Thread(target=context.server.serve_forever)
     context.pa_app.start()
 
-    context.browser = webdriver.Chrome(options=chrome_options, executable_path='/home/travis/virtualenv/python3.8.2/bin/chromedriver')
+    context.browser = webdriver.Chrome()#options=chrome_options, executable_path='/home/travis/virtualenv/python3.8.2/bin/chromedriver')
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 
