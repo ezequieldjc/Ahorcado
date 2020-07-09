13a389e953effdcfa2caf0216814ac2b1862c3b2
gabicapitanelli <gabrielcapitanelli@hotmail.com>
2020-07-09 18:57:02 -0300

    Actualizacion de credenciales DB Postgres-Heroku

--- project/__init__.py
+++ project/__init__.py
@@ -7,9 +7,8 @@ def create_app(config_name):
 
     app.secret_key = "1234"
 
-    app.config['SECRET_KEY'] = '1eab0cc55df07f487a356cc0df610aa365d6d69aaaf1eb399da61f20104d06aa'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qnmpruhxfeoklo:1eab0cc55df07f487a356cc0df610aa365d6d69aaaf1eb399da61f20104d06aa@ec2-52-0-155-79.compute-1.amazonaws.com:5432/d7k9i0f2ad0li0'
-
+    app.config['SECRET_KEY'] = 'e826016dbfecf5254f9aada20205c4448bab4d2f321c2bbca85c624640d7fb18'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xpfghngjctzyyv:e826016dbfecf5254f9aada20205c4448bab4d2f321c2bbca85c624640d7fb18@ec2-50-16-198-4.compute-1.amazonaws.com:5432/d2ajllg55s6ehd'
     db.init_app(app)
 
     with app.app_context():
